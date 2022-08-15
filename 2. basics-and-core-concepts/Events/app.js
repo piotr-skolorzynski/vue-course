824ff8aee47e3320142aca3f1f26f211464381e5
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-08-15 11:25:51 +0200

    events and methods

--- 2. basics-and-core-concepts/Events/app.js
+++ 2. basics-and-core-concepts/Events/app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add() {
+      this.counter += 1;
+    },
+    substract() {
+      this.counter -= 1;
+    }
+  }
 });
 
 app.mount('#events');
