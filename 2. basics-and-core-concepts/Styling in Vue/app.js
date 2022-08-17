5ab652be046677e66df9013f9667f792edf96e52
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-08-17 21:46:26 +0200

    create directory Stylin in Vue with index.html styles.css and app.js fill add example of dynamic styling with inline styles

---
+++ 2. basics-and-core-concepts/Styling in Vue/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false,
+        }
+    },
+    methods: {
+        boxSelected(box) {
+            if (box === "A") {
+                this.boxASelected = !this.boxASelected;
+            }
+
+            if (box === "B") {
+                this.boxBSelected = !this.boxBSelected;
+            }
+
+            if (box === "C") {
+                this.boxCSelected = !this.boxCSelected;
+            }
+        }
+    }
+});
+
+app.mount("#styling");
