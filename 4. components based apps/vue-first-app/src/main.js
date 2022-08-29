6e198bc5118924d353f089423bd099ac92cb57f3
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-08-29 21:28:56 +0200

    create main component App and FriendContact component combine them and add styling

--- 4. components based apps/vue-first-app/src/main.js
+++ 4. components based apps/vue-first-app/src/main.js
@@ -1,3 +1,9 @@
 import { createApp } from 'vue';
+import App from './App.vue';
+import FriendContact from './components/FriendContact.vue';
 
-createApp({}).mount('');
+const app = createApp(App);
+
+app.component('friend-contact', FriendContact);
+
+app.mount('#app');
