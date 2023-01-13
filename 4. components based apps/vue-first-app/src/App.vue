7aaaae76169d64400a3ce26453a81e69e932cd8f
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-13 17:26:11 +0100

    create NewFriend component with content to add new contact create methods to send contact data modify App component to listen to passed contact data and add new contact to frriends array

--- 4. components based apps/vue-first-app/src/App.vue
+++ 4. components based apps/vue-first-app/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My friends</h1>
     </header>
+    <new-friend @add-contact="onAddContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -20,8 +21,9 @@
 
 <script>
 import FriendContact from './components/FriendContact.vue';
+import NewFriend from './components/NewFriend.vue';
 export default {
-  components: { FriendContact },
+  components: { FriendContact, NewFriend },
   data() {
     return {
       friends: [
@@ -46,6 +48,14 @@ export default {
     toggleFavouriteStatus(id) {
       const identifiedFriend = this.friends.find((friend) => friend.id === id);
       identifiedFriend.isFavourite = !identifiedFriend.isFavourite;
+    },
+
+    onAddContact(friend) {
+      this.friends.push({
+        ...friend,
+        id: crypto.randomUUID(),
+        isFavourite: false,
+      });
     },
   },
 };
@@ -84,7 +94,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -109,6 +120,7 @@ header {
   color: white;
   padding: 0.05rem 1rem;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+  margin: auto;
 }
 
 #app button:hover,
@@ -116,5 +128,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ 4. components based apps/vue-first-app/src/components/NewFriend.vue
@@ -0,0 +1,49 @@
+<template>
+  <form @submit.prevent="onAddNewContact">
+    <div>
+      <label for="name">Name:</label>
+      <input type="text" ref="name" id="name" />
+    </div>
+    <div>
+      <label for="phone">Phone Number:</label>
+      <input type="tel" ref="phone" id="phone" />
+    </div>
+    <div>
+      <label for="email">Email address:</label>
+      <input type="email" ref="email" id="email" />
+    </div>
+    <div>
+      <button>Add contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  emits: ['add-contact'],
+  methods: {
+    onAddNewContact() {
+      if (
+        !this.$refs.name.value ||
+        !this.$refs.phone.value ||
+        !this.$refs.email.value
+      ) {
+        return;
+      }
+
+      this.$emit('add-contact', {
+        name: this.$refs.name.value,
+        phone: this.$refs.phone.value,
+        email: this.$refs.email.value,
+      });
+
+      this.$refs.name.value = '';
+      this.$refs.phone.value = '';
+      this.$refs.email.value = '';
+    },
+  },
+};
+</script>
