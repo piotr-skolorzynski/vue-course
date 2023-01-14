78215a90e88cd339117049fd0a9407a71c24e244
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-14 14:02:07 +0100

    add example how to provide and use reference to a method from parent component to execute in child component instead of sending custom event and executing this method after it was sent

--- 4. components based apps/second-app/src/App.vue
+++ 4. components based apps/second-app/src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
@@ -33,7 +33,7 @@ export default {
     };
   },
   provide() {
-    return { topics: this.topics };
+    return { topics: this.topics, selecteTopic: this.activateTopic };
   },
   methods: {
     activateTopic(topicId) {
--- 4. components based apps/second-app/src/components/KnowledgeElement.vue
+++ 4. components based apps/second-app/src/components/KnowledgeElement.vue
@@ -2,13 +2,13 @@
   <li>
     <h3>{{ topicName }}</h3>
     <p>{{ description }}</p>
-    <button @click="$emit('select-topic', id)">Learn More</button>
+    <button @click="selectTopic(id)">Learn More</button>
   </li>
 </template>
 
 <script>
 export default {
   props: ['id', 'topicName', 'description'],
-  emits: ['select-topic'],
+  inject: ['selectTopic'],
 };
 </script>
