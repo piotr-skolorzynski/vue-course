8f5e9bd8750fcbcc778b1d6861cfc1405608ac6e
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-15 16:28:50 +0100

    create CourseGoals component to show how scoped slots work

--- 5. components deep dive/src/App.vue
+++ 5. components deep dive/src/App.vue
@@ -7,6 +7,9 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+    </course-goals>
   </div>
 </template>
 
@@ -14,9 +17,10 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
-  components: { TheHeader, BadgeList, UserInfo },
+  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
   data() {
     return {
       activeUser: {
---
+++ 5. components deep dive/src/components/CourseGoals.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot v-bind:item="goal"></slot>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      goals: ['Finish the course', 'Learn Vue'],
+    };
+  },
+};
+</script>
