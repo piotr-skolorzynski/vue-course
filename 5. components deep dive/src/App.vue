96847a5b0addaea559832e18070ba52f4bb78e3a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-15 17:15:47 +0100

    add more  about dynamic components show how to use keep-alive to save  data from dynamic components

--- 5. components deep dive/src/App.vue
+++ 5. components deep dive/src/App.vue
@@ -1,34 +1,36 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">ManageGoals</button>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
-  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
+  components: { TheHeader, ActiveGoals, ManageGoals },
   data() {
     return {
+      selectedComponent: 'active-goals',
+
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
+  },
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component;
+    },
   },
 };
 </script>
