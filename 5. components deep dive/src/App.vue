8e6449e614b592e5f96cf79770e8fc29af0d4ade
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-15 10:44:36 +0100

    create directory components deep dive with project add example how to add components locally

---
+++ 5. components deep dive/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default {
+  components: {
+    'the-header': TheHeader,
+    'badge-list': BadgeList,
+    'user-info': UserInfo,
+  },
+  // components: {
+  //   TheHeader: TheHeader,
+  //   BadgeList: BadgeList,
+  //   UserInfo: UserInfo,
+  // },
+  // components: { TheHeader, BadgeList, UserInfo }, //nowoczesna składania dla tej samej nazwy klucz:wartość
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
