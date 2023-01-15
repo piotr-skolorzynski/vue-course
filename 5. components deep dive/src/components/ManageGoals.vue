bf472c4a8d5151bbdfb29d9907836d39efb98b78
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-15 21:41:30 +0100

    add example of using teleports

--- 5. components deep dive/src/components/ManageGoals.vue
+++ 5. components deep dive/src/components/ManageGoals.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="userGoal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Ok</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
+<script>
+import { ErrorAlert } from './ErrorAlert.vue';
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.userGoal.value;
+      if (enteredValue === '') {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
