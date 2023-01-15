4ee109e106a29aa21fceac4f0c271560e98931c2
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-15 12:38:27 +0100

    create BaseCard component to show an idea of using slots

--- 5. components deep dive/src/components/UserInfo.vue
+++ 5. components deep dive/src/components/UserInfo.vue
@@ -1,31 +1,19 @@
 <template>
   <section>
-    <header>
+    <base-card>
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+      <p>{{ infoText }}</p>
+    </base-card>
   </section>
 </template>
 
 <script>
+import BaseCard from './BaseCard.vue';
 export default {
+  components: { BaseCard },
   props: ['fullName', 'infoText', 'role'],
 };
 </script>
 
-<style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
+<style scoped></style>
