6508d1dfa4e86199fbed32468886e12508383c9a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-16 21:05:17 +0100

    add form in AddResource component

--- 6. The Learning Resources App/src/components/learning-resources/AddResource.vue
+++ 6. The Learning Resources App/src/components/learning-resources/AddResource.vue
@@ -1,9 +1,53 @@
 <template>
-  <h2>Add Resource</h2>
+  <BaseCard>
+    <form @submit.prevent="">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea id="description" name="description" rows="3"></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input id="link" name="link" type="url" />
+      </div>
+      <div>
+        <BaseButton type="submit">Add Resource</BaseButton>
+      </div>
+    </form>
+  </BaseCard>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
