0c09209d474ac35aaa405a8d3fc50eb759d6923f
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-16 13:49:51 +0100

    create LearningResource compoenent as a list item use it in App component

---
+++ 6. The Learning Resources App/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <learning-resources
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resources>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  data() {
+    return {
+      components: { LearningResource },
+      storedResources: [
+        {
+          id: crypto.randomUUID(),
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: crypto.randomUUID(),
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
