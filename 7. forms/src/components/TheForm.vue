5cbb333e5e8ad6ce57d859774389257a5d784678
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-17 13:54:02 +0100

    add example of basic validation

--- 7. forms/src/components/TheForm.vue
+++ 7. forms/src/components/TheForm.vue
@@ -1,13 +1,18 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: userNameValidity === 'invalid' }"
+    >
       <label for="user-name">Your Name</label>
       <input
         id="user-name"
         name="user-name"
         type="text"
         v-model.trim="userName"
+        @blur="validateInput"
       />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -112,6 +117,7 @@ export default {
       interest: [],
       how: null,
       confirm: false,
+      userNameValidity: 'pending',
     };
   },
   methods: {
@@ -122,6 +128,13 @@ export default {
       this.interest = [];
       this.how = null;
       this.confirm = false;
+    },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     },
   },
 };
@@ -139,6 +152,13 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+.form-control.invalid label {
+  color: red;
 }
 
 label {
