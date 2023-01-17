9bdb06ee254ac1f4525ddbf8f515a4bf4ac1d1d8
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-17 12:03:41 +0100

    v-model and inputs

--- 7. forms/src/components/TheForm.vue
+++ 7. forms/src/components/TheForm.vue
@@ -1,12 +1,17 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+      />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model="userAge" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
@@ -51,6 +56,25 @@
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userAge: null,
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('Username: ' + this.userName);
+      this.userName = '';
+
+      this.userAge = null;
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
