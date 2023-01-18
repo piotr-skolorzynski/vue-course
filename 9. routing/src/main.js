2bf486191f608d3e1ddb9a3039c8c3024c9f748d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-18 20:00:09 +0100

    style active links

--- 9. routing/src/main.js
+++ 9. routing/src/main.js
@@ -14,6 +14,7 @@ const router = createRouter({
         { path: '/teams', component: TeamsList },
         { path: '/users', component: UsersList },
     ],
+    linkActiveClass: 'active'
 });
 
 const app = createApp(App);
