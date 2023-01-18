4113d006c2357337a88e77a0e5fd53dfd603efd1
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-18 12:28:42 +0100

    handle backend error responses

--- 8. http-requests/src/components/survey/LearningSurvey.vue
+++ 8. http-requests/src/components/survey/LearningSurvey.vue
@@ -41,6 +41,7 @@
         <p v-if="invalidInput">
           One or more input fields are invalid. Please check your provided data.
         </p>
+        <p v-if="error">{{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -56,6 +57,7 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null,
     };
   },
   methods: {
@@ -65,6 +67,7 @@ export default {
         return;
       }
       this.invalidInput = false;
+      this.error = null;
 
       fetch(
         'https://vue-course-http-requests-d7100-default-rtdb.europe-west1.firebasedatabase.app/surveys.json',
@@ -78,7 +81,15 @@ export default {
             rating: this.chosenRating,
           }),
         }
-      );
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Could not save data!');
+          }
+        })
+        .catch((error) => {
+          this.error = error.message;
+        });
 
       this.enteredName = '';
       this.chosenRating = null;
