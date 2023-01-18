1ad0cec27e39f65d9fdd3e37ffb3fcec85c612b0
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-18 19:36:53 +0100

    create paths register router and set router-view in App to place components renders using router

--- 9. routing/src/main.js
+++ 9. routing/src/main.js
@@ -2,15 +2,22 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
     // tells how to manage the routing history, there are two types createWebHistory means 
     // that router should use build in browser mechanism of history
     history: createWebHistory(),
     //registry of routes to be used
-    routes: [],
+    routes: [
+        { path: '/teams', component: TeamsList },
+        { path: '/users', component: UsersList },
+    ],
 });
 
 const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
