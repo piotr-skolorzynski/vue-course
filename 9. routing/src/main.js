16846c791d51365fee7d89340e68e92c65c503f1
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-18 19:19:38 +0100

    routing setup

--- 9. routing/src/main.js
+++ 9. routing/src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 
-const app = createApp(App)
+const router = createRouter({
+    // tells how to manage the routing history, there are two types createWebHistory means 
+    // that router should use build in browser mechanism of history
+    history: createWebHistory(),
+    //registry of routes to be used
+    routes: [],
+});
+
+const app = createApp(App);
 
 app.mount('#app');
