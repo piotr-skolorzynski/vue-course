ee2132896f86bb15155a4afdb8968c7f6dd16875
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-19 21:15:31 +0100

    redirec and catch all routes

--- 9. routing/src/main.js
+++ 9. routing/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
     // tells how to manage the routing history, there are two types createWebHistory means 
@@ -12,9 +13,11 @@ const router = createRouter({
     history: createWebHistory(),
     //registry of routes to be used
     routes: [
+        { path: '/', redirect: '/teams' },
         { path: '/teams', component: TeamsList },
         { path: '/users', component: UsersList },
-        { path: '/teams/:teamId', component: TeamMembers, props: true }
+        { path: '/teams/:teamId', component: TeamMembers, props: true },
+        { path: '/:not-found(.*)', name: 'NotFound', component: NotFound }
     ],
     linkActiveClass: 'active'
 });
