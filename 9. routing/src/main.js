81c118f5d96bc28bc547f478afd3e95e43300f37
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-21 13:30:10 +0100

    render multipe routes with named router-views tags

--- 9. routing/src/main.js
+++ 9. routing/src/main.js
@@ -6,6 +6,8 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
     // tells how to manage the routing history, there are two types createWebHistory means 
@@ -15,12 +17,12 @@ const router = createRouter({
     routes: [
         { path: '/', redirect: '/teams' },
         {
-            path: '/teams', name: 'teams', component: TeamsList, children:
+            path: '/teams', name: 'teams', components: { default: TeamsList, footer: TeamsFooter }, children:
                 [
-                    { path: ':teamId', name: 'team-members', component: TeamMembers, props: true }, //teams/t1
+                    { path: ':teamId', name: 'team-members', component: TeamMembers, props: true },
                 ]
         },
-        { path: '/users', component: UsersList },
+        { path: '/users', components: { default: UsersList, footer: UsersFooter } },
         { path: '/:not-found(.*)', component: NotFound }
     ],
     linkActiveClass: 'active'
