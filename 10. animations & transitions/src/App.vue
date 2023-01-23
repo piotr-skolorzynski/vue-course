876a0cfcdc34701e4fac7c100a0ebcbc7d8b3ddd
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-23 22:53:50 +0100

    use transition component with animation

--- 10. animations & transitions/src/App.vue
+++ 10. animations & transitions/src/App.vue
@@ -79,30 +79,36 @@ button:active {
 }
 
 .v-enter-from {
-  opacity: 0;
-  transform: translateY(-30px);
 }
 
 .v-enter-active {
-  transition: all 0.3s ease-out;
+  animation: slide-scale 0.3s ease-out;
 }
 
 .v-enter-to {
-  opacity: 1;
-  transform: translateY(0);
 }
 
 .v-leave-from {
-  opacity: 1;
-  transform: translateY(0);
 }
 
 .v-leave-active {
-  transition: all 0.3s ease-out;
+  animation: slide-scale 0.3s ease-out;
 }
 
 .v-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
+}
+
+@keyframes slide-scale {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
