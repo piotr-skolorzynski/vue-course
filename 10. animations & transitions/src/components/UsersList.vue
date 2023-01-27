4ac0a2905398d8d6402a7c771a3c39ce0041742b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-27 17:48:33 +0100

    animate list items

--- 10. animations & transitions/src/components/UsersList.vue
+++ 10. animations & transitions/src/components/UsersList.vue
@@ -61,9 +61,13 @@ li {
 }
 .user-list-leave-active {
   transition: all 1s ease-out;
+  position: absolute;
 }
 .user-list-leave-to {
   opacity: 0;
   transform: translateX(30px);
+}
+.user-list-move {
+  transition: transform 0.8s ease;
 }
 </style>
