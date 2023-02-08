51f05e8ae7d4fd5a43a658ac6a7bf8009d79c5db
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-08 19:35:04 +0100

    build CoachDetail component

--- 13. Project Find Coach/src/router.js
+++ 13. Project Find Coach/src/router.js
@@ -12,7 +12,7 @@ const router = createRouter({
         { path: '/', redirect: '/coaches' },
         { path: '/coaches', component: CoachesList },
         {
-            path: '/coaches/:id', component: CoachDetail, children: [
+            path: '/coaches/:id', component: CoachDetail, props: true, children: [
                 { path: 'contact', component: ContactCoach } // /coaches/c1/contact
             ]
         },
