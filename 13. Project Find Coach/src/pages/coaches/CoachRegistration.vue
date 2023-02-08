bd665db55c5ed6c703bbc2b05996a57451d04a08
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-08 21:20:06 +0100

    Register as a Coach create form

--- 13. Project Find Coach/src/pages/coaches/CoachRegistration.vue
+++ 13. Project Find Coach/src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,16 @@
-<template>Register</template>
+<template>
+  <section>
+    <BaseCard>
+      <h2>Register as a coach now!</h2>
+      <CoachForm />
+    </BaseCard>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: { CoachForm },
+};
+</script>
