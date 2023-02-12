3a3ee69056a07c60a5cc07255a052d78119942d7
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-12 19:45:19 +0100

    fetch coach data gethttp request

--- 13. Project Find Coach/src/pages/coaches/CoachesList.vue
+++ 13. Project Find Coach/src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <BaseCard>
       <div class="controls">
-        <BaseButton mode="outline">Refresh</BaseButton>
+        <BaseButton mode="outline" @click="loadCoaches">Refresh</BaseButton>
         <BaseButton v-if="isCoach" link to="/register"
           >Register as a Coach</BaseButton
         >
@@ -65,9 +65,15 @@ export default {
       return this.$store.getters['coaches/hasCoaches'];
     },
   },
+  created() {
+    this.loadCoaches();
+  },
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
     },
   },
 };
--- 13. Project Find Coach/src/store/modules/coaches/actions.js
+++ 13. Project Find Coach/src/store/modules/coaches/actions.js
@@ -25,5 +25,30 @@ export default {
             ...coachData,
             id: userId
         });
+    },
+    async loadCoaches(context) {
+        const response = await fetch(`https://vue-course-http-requests-d7100-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`);
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            // error handling
+        }
+
+        const coaches = [];
+
+        for (const key in responseData) {
+            const coach = {
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas,
+                id: key
+            };
+
+            coaches.push(coach);
+        };
+
+        context.commit('coaches', coaches);
     }
 }
