caaf0b5d1e9e900233b66c4eab77b7babb577363
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-12 18:27:15 +0100

    send put request to store coach data in firebase

--- 13. Project Find Coach/src/store/modules/coaches/actions.js
+++ 13. Project Find Coach/src/store/modules/coaches/actions.js
@@ -1,14 +1,29 @@
 export default {
-    registerCoach(context, payload) {
+    async registerCoach(context, payload) {
+        const userId = context.rootGetters.userId;
+
         const coachData = {
-            id: context.rootGetters.userId,
             firstName: payload.first,
             lastName: payload.last,
             description: payload.desc,
             hourlyRate: payload.rate,
             areas: payload.areas
+        };
+
+        const response = await fetch(`https://vue-course-http-requests-d7100-default-rtdb.europe-west1.firebasedatabase.app/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        });
+
+        // const responseData = await response.json();
+
+        if (!response.ok) {
+            // error handling
         }
 
-        context.commit('registerCoach', coachData);
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId
+        });
     }
 }
