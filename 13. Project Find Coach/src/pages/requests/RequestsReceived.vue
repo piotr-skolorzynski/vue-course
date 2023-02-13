17e9932281c2eff8f4b2f76d4ab0c7f2a0ec1bef
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-13 23:11:21 +0100

    Send coaching requests via https requests

--- 13. Project Find Coach/src/pages/requests/RequestsReceived.vue
+++ 13. Project Find Coach/src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+  <BaseDialog :show="!!error" title="An error occurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </BaseDialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <BaseSpinner v-if="isLoading" />
+      <ul v-else-if="hasRequests && !isLoading">
         <RequestItem
           v-for="request in receivedRequests"
           :key="request.id"
@@ -24,12 +28,35 @@ export default {
   components: {
     RequestItem,
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
     },
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
--- 13. Project Find Coach/src/store/modules/requests/actions.js
+++ 13. Project Find Coach/src/store/modules/requests/actions.js
@@ -1,12 +1,51 @@
 export default {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
         const newRequest = {
-            id: crypto.randomUUID(),
-            coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
         };
 
+        const response = await fetch(`https://vue-course-http-requests-d7100-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to send request.');
+            throw error;
+        }
+
+        newRequest.id = responseData.name; //firebase
+        newRequest.coachId = payload.coachId;
+
         context.commit('addRequest', newRequest);
     },
+    async fetchRequests(context) {
+        const coachId = context.rootGetters.userId;
+        const response = await fetch(`https://vue-course-http-requests-d7100-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`);
+
+        const responseData = await resposne.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to fetch requests!');
+            throw error;
+        }
+
+        const requests = [];
+
+        for (const key in responseData) {
+            const request = {
+                id: key,
+                coachId,
+                userEmail: responseData[key].userEmail,
+                message: responseData[key].message
+            };
+
+            requests.push(request);
+        }
+
+        context.commit('setRequests', requests);
+    }
 };
