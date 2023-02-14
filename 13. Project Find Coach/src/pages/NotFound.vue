deae76164e0090b1e4636825bb4466cd3516a733
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-14 14:55:23 +0100

    not-found page

---
+++ 13. Project Find Coach/src/pages/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <base-card>
+    <h2>Page not found</h2>
+    <p>
+      This page could not be found - maybe check out all our
+      <router-link to="/coaches">couches</router-link>.
+    </p>
+  </base-card>
+</template>
