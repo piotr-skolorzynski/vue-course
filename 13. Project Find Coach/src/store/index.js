dac7e9801623fbe6e5b46c03bc955ccd1f7e5f79
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-16 21:52:23 +0100

    prepare auth module in vuex store

--- 13. Project Find Coach/src/store/index.js
+++ 13. Project Find Coach/src/store/index.js
@@ -1,22 +1,14 @@
 import { createStore } from 'vuex';
 import coachesModule from './modules/coaches';
 import requestsModule from './modules/requests'
+import authModule from './modules/auth/';
 
 const store = createStore({
     modules: {
         coaches: coachesModule,
-        requests: requestsModule
+        requests: requestsModule,
+        auth: authModule
     },
-    state() {
-        return {
-            userId: 'c3'
-        }
-    },
-    getters: {
-        userId(state) {
-            return state.userId;
-        }
-    }
 });
 
 export default store;
