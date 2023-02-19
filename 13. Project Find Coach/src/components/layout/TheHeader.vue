7ac37b6ea3996b9d2f9149a4a7c64b4454a5493a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-19 19:46:24 +0100

    add logout action flow

--- 13. Project Find Coach/src/components/layout/TheHeader.vue
+++ 13. Project Find Coach/src/components/layout/TheHeader.vue
@@ -10,6 +10,9 @@
         <li v-else>
           <router-link to="/auth">Login</router-link>
         </li>
+        <li v-if="isLoggedIn">
+          <base-button @click="logout">Logout</base-button>
+        </li>
       </ul>
     </nav>
   </header>
@@ -20,6 +23,11 @@ export default {
   computed: {
     isLoggedIn() {
       return this.$store.getters.isAuthenticated;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout');
     },
   },
 };
