49ad0a6c1b82c4b345092b1c2be57145fd9618ac
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-19 11:16:32 +0100

    Add signup action flow

--- 13. Project Find Coach/src/store/modules/auth/actions.js
+++ 13. Project Find Coach/src/store/modules/auth/actions.js
@@ -1,1 +1,29 @@
-export default {};
+export default {
+    login() {
+
+    },
+    async signup(context, payload) {
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAUTnSd1oUTBfC4JNYz5WdmDHQRmPTYI_I', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            console.log(responseData);
+            const error = new Error(responseData.message) || 'Failed to authenticat';
+            throw error;
+        }
+
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiration: responseData.expiresIn
+        })
+    }
+};
--- 13. Project Find Coach/src/store/modules/auth/index.js
+++ 13. Project Find Coach/src/store/modules/auth/index.js
@@ -5,7 +5,9 @@ import getters from "./getters";
 export default {
     state() {
         return {
-            userId: 'c3'
+            userId: null,
+            token: null,
+            tokenExpiration: null
         }
     },
     mutations,
