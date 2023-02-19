81e9db02b2fabea0cad64bcced496c381fba7459
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-19 18:41:14 +0100

    add login action flow

--- 13. Project Find Coach/src/store/modules/auth/actions.js
+++ 13. Project Find Coach/src/store/modules/auth/actions.js
@@ -1,9 +1,30 @@
 export default {
-    login() {
+    async login(context, payload) {
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAUTnSd1oUTBfC4JNYz5WdmDHQRmPTYI_I', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        });
 
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            console.log(responseData);
+            const error = new Error(responseData.message) || 'Failed to authenticate. Check your login data.';
+            throw error;
+        }
+
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiration: responseData.expiresIn
+        });
     },
     async signup(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAUTnSd1oUTBfC4JNYz5WdmDHQRmPTYI_I', {
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAUTnSd1oUTBfC4JNYz5WdmDHQRmPTYI_I', {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
