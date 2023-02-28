731d5a92a6964768400486d2a91788edb5d45b98
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-02-28 22:34:22 +0100

    emit custom event

--- 15. composition API/src/components/UserData.vue
+++ 15. composition API/src/components/UserData.vue
@@ -1,14 +1,17 @@
 <template>
   <h2>{{ userName }}</h2>
   <h3>{{ age }}</h3>
+  <button @click="saveData">Save Data</button>
 </template>
 
 <script setup>
-import { defineProps, computed } from 'vue';
+import { defineProps, computed, defineEmits } from 'vue';
 
 const props = defineProps(['firstName', 'lastName', 'age']);
 
 const userName = computed(() => {
   return props.firstName + ' ' + props.lastName;
 });
+
+const saveData = defineEmits([('save', { userName, age })]);
 </script>
