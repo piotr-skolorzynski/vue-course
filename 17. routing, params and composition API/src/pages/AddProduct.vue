17cd3f92723eb5e83bd689c74492420744da5f80
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-03-11 18:00:57 +0100

    the route $ router object and the composition API

--- 17. routing, params and composition API/src/pages/AddProduct.vue
+++ 17. routing, params and composition API/src/pages/AddProduct.vue
@@ -12,7 +12,11 @@
       </div>
       <div>
         <label for="description">Description</label>
-        <textarea rows="5" id="description" v-model="enteredDescription"></textarea>
+        <textarea
+          rows="5"
+          id="description"
+          v-model="enteredDescription"
+        ></textarea>
       </div>
       <button>Add Product</button>
     </form>
@@ -21,10 +25,12 @@
 
 <script>
 import { ref, inject } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default {
   setup() {
     const addProduct = inject('addProduct');
+    const router = useRouter();
 
     const enteredTitle = ref('');
     const enteredPrice = ref(null);
@@ -36,6 +42,8 @@ export default {
         description: enteredDescription,
         price: enteredPrice,
       });
+
+      router.push('/products');
     }
 
     return {
